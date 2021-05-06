25b6782c92b986ea5771f15fe87211c6a6038c6c
chornr <32269887+chornr@users.noreply.github.com>
2021-05-06 14:14:28 +0300

    3. Calculations

---
+++ 3.11 sum_of_digits.py
@@ -0,0 +1,6 @@
+# coding=utf-8
+# Дано трехзначное число. Найдите сумму его цифр.
+
+n = int(input())
+
+print(int(str(n)[0])+int(str(n)[1])+int(str(n)[2]))
---
+++ 3.12 hypot.py
@@ -0,0 +1,8 @@
+# coding=utf-8
+# Дано два числа a и b. Выведите гипотенузу треугольника с заданными катетами.
+
+import math
+a = int(input())
+b = int(input())
+
+print(math.sqrt(a*a+b*b))
