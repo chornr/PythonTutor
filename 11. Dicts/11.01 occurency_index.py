9c79b61f909d3997383ef5518f2fa81467211fc4
Roman Chornyi <romchablack2@gmail.com>
2023-04-14 12:17:18 +0100

    Added ChatGPT's solution

--- 11. Dicts/11.01 occurency_index.py
+++ 11. Dicts/11.01 occurency_index.py
@@ -1,12 +1,22 @@
 # Условие
-#
 # В единственной строке записан текст. Для каждого слова из данного текста подсчитайте, сколько раз оно
 # встречалось в этом тексте ранее.
-#
 # Словом считается последовательность непробельных символов идущих подряд, слова разделены одним или большим числом
 # пробелов или символами конца строки.
 
-counter = {}
+# My solution
+counter = {}  # инициализируем пустой словарь
 for word in input().split():
     counter[word] = counter.get(word, 0) + 1
     print(counter[word] - 1, end=' ')
+
+# ChatGPT solution
+text = input() # вводим текст как одну строку
+word_count = {} # инициализируем пустой словарь
+words = text.split() # разбиваем строку на слова
+for word in words:
+    if word in word_count:
+        word_count[word] += 1
+    else:
+        word_count[word] = 0
+    print(word_count[word], end=" ")
