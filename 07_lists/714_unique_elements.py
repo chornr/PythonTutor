f845b01a3a5adac39766e7948f14b9d6e4cd6bfa
Roman Chornyi <32269887+romchablack@users.noreply.github.com>
2023-08-30 14:53:39 +0100

    Update 714_unique_elements.py

--- 07_lists/714_unique_elements.py
+++ 07_lists/714_unique_elements.py
@@ -1,13 +1,11 @@
-# coding=utf-8 Дан список. Выведите те его элементы, которые встречаются в списке только один раз. Элементы нужно
-# выводить в том порядке, в котором они встречаются в списке.
+# coding=utf-8 Дан список. Выведите те его элементы, которые встречаются в списке только один раз. 
+# Элементы нужно выводить в том порядке, в котором они встречаются в списке.
 
-from typing import List
+L = [int(i) for i in input().split()]
+L1 = []
 
-L = [int(i) for i in input().split()]  # type: List[int]
-count = 0
+for element in L:
+    if L.count(element) == 1:
+        L1.append(str(element))
 
-for i in range(len(L)):
-    for j in range(i+1, len(L)):
-        if L[i] == L[j]:
-            count += 1
-print(count)
+print(" ".join(L1))
