011ba3ce1b5bf18a93366d00822b6383754ec2c6
Roman Chornyi <32269887+romchablack@users.noreply.github.com>
2023-09-04 19:36:46 +0100

    Update 1112_genealogy_ancestors_and_descendants.py
    Updated resolution

--- 11_dicts/1112_genealogy_ancestors_and_descendants.py
+++ 11_dicts/1112_genealogy_ancestors_and_descendants.py
@@ -5,6 +5,55 @@
 #
 # Для каждого такого запроса выведите одно из трех чисел: 1, если первый элемент является предком второго, 2, если второй является предком первого или 0, если ни один из них не является предком другого.
 
+# CORRECT
+n = int(input())
+family = {}
+family_tree = {}
+
+for i in range(n-1):
+    kid, parent = input().split()
+    family[kid] = parent
+    
+while len(family_tree) != n:
+    for kid, parent in family.items():
+        if parent not in family.keys():
+            family_tree[parent] = 0
+            family_tree[kid] = 1
+        elif parent in family_tree:
+            family_tree[kid] = family_tree[parent] + 1
+            
+m = int(input())
+results = []
+
+for i in range(m):
+    name_1, name_2 = input().split()
+    height = family_tree[name_1] - family_tree[name_2]
+        
+    if height < 0:
+        while height < 0:
+            name_2 = family[name_2]
+            height = height + 1
+        if name_2 == name_1:
+            results.append("1")
+        else:
+            results.append("0")
+
+    elif height > 0:
+        while height > 0:
+            name_1 = family[name_1]
+            height = height - 1
+        if name_1 == name_2:
+            results.append("2")
+        else:
+            results.append("0")
+    
+    else:
+        results.append("0")
+
+print(" ".join(results))
+
+
+# NOT COMPLETED
 def dicts_genealogy_ancestors_and_descendants():
     N = int(input())
     PARENTS = []
